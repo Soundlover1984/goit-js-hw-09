a277bab542086e8087c8f28ef5d2b0b69749cc19
Soundlover1984 <denys.kovtun@gmail.com>
2023-04-07 22:56:42 +0300

    switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const bodyChangeColor = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+let intervalValue = 0;
+
+intervalValue = setInterval(() => {
+  bodyChangeColor.style.backgroundColor = getRandomHexColor();
+}, 1000);
+
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', (element) => {
+  element.target.disabled = true;
+  buttonStop.disabled = false;
+});
+
+buttonStop.addEventListener('click', (element) => {
+  element.target.disabled = true;
+  buttonStart.disabled = false;
+  clearInterval(intervalValue);
+});
+
 
