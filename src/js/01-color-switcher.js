636f614a08dd4d6f49661ee8e413c93c34e755b2
Soundlover1984 <denys.kovtun@gmail.com>
2023-04-07 23:11:32 +0300

    switcher rebuild

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,28 +1,22 @@
+
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
-const bodyChangeColor = document.querySelector('body');
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-}
-
 let intervalValue = 0;
 
-intervalValue = setInterval(() => {
-  bodyChangeColor.style.backgroundColor = getRandomHexColor();
-}, 1000);
+function changeColor() {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+}
 
-buttonStop.disabled = true;
-
-buttonStart.addEventListener('click', (element) => {
-  element.target.disabled = true;
-  buttonStop.disabled = false;
+buttonStart.addEventListener('click', function() {
+  intervalValue = setInterval(changeColor, 1000);
+  buttonStart.setAttribute('disabled', 'true');
+  buttonStop.removeAttribute('disabled');
 });
 
-buttonStop.addEventListener('click', (element) => {
-  element.target.disabled = true;
-  buttonStart.disabled = false;
+buttonStop.addEventListener('click', function() {
   clearInterval(intervalValue);
+  buttonStop.setAttribute('disabled', 'true');
+  buttonStart.removeAttribute('disabled');
 });
 
 
