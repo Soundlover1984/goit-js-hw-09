7056e77e94ef3ffbff2796cf2d1ef05029f3bf29
Soundlover1984 <116640926+Soundlover1984@users.noreply.github.com>
2023-04-08 16:07:23 +0000

    Deploying to gh-pages from @ Soundlover1984/goit-js-hw-09@7995dd6a5815f902c0b849e6cbdfe860c7f013d1 🚀

---
+++ 01-color-switcher.9e83e99b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;const a=()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`};t.addEventListener("click",(()=>{d=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(d),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9e83e99b.js.map
