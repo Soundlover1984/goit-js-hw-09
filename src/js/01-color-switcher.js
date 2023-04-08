ed8e6bb51cdaa7506b531fde9c89d6e3e93baf6f
Denys Kovtun <denys.kovtun@gmail.com>
2023-04-08 10:58:48 +0300

    switcher fix 2.0

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,6 @@
-
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
-let intervalValue = 0;
+let intervalValue = null;
 
 function changeColor() {
   document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
@@ -18,5 +17,34 @@ buttonStop.addEventListener('click', function() {
   buttonStop.setAttribute('disabled', 'true');
   buttonStart.removeAttribute('disabled');
 });
+
+// 2nd variant
+
+
+// const buttonStart = document.querySelector('button[data-start]');
+// const buttonStop = document.querySelector('button[data-stop]');
+// let intervalValue = null;
+
+// function enableDisableButtons(startBtnDisabled, stopBtnDisabled) {
+//   buttonStart.disabled = startBtnDisabled;
+//   buttonStop.disabled = stopBtnDisabled;
+// }
+
+// function changeColor() {
+//   document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, '0')}`;
+// }
+
+// buttonStart.addEventListener('click', function () {
+//   intervalValue = setInterval(changeColor, 1000);
+//   enableDisableButtons(true, false);
+// });
+
+// buttonStop.addEventListener('click', function () {
+//   clearInterval(intervalValue);
+//   enableDisableButtons(false, true);
+// });
+
 
 
